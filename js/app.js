7bcff6d546ced5b27fb9c318e4664ed9bc01fcdf
Jefferson Gakuya <jeffersongakuya@gmail.com>
2020-12-14 23:22:49 +0300

    increased years to 1900

--- js/app.js
+++ js/app.js
@@ -13,20 +13,17 @@ function calc() {
     var CC = year.slice(0, 2);
     var YY = year.slice(2, 4);
 
-    if (DD === "" || DD <= 0 || DD > 31 && MM === "" || MM <= 0 || MM > 12 && year === "" || year.length > 4) {
+    if (DD === "" || DD <= 0 || DD > 31 && MM === "" || MM <= 0 || MM > 12 && year < 1900 || year === "" || year.length > 4) {
         alert("Please, make sure you have entered the correct information!!")
     } else {
         var days = Math.floor((((CC /  4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
     }
 
 
-
     if (gender === 'male'){ 
         document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[days] + ".");
-    
     } else if (gender === 'female') {
         document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[days] + ".");
-    
     } else {
         alert("I'm sorry to inform you, you never choose the gender!");
     }
