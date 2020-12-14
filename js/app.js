1ae2cbd3a506bd0166eadc4eb005ef99c3b4ce81
Jefferson Gakuya <jeffersongakuya@gmail.com>
2020-12-14 05:51:41 +0300

    added get the day function

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 function calc() {
     event.preventDefault();
-    var day = document.getElementById('day');
+    var days = document.getElementById('day');
     var year = document.getElementById('year');
     var month = document.getElementById('month');
     var gender = document.getElementById('gender').value;
-    dates = new Date(year);
-    var days = dates.getDay();
+    date = new Date(eval(year).value);
+    // var days = dates.getDay();
+
+    function getTheDay() {
+        date = new Date(eval(year).value);
+        // return days[myDate.getDay()]
+    }
 
     // Invalid Day
     if ((day <= 0) ||  (day > 31)) {
@@ -28,14 +33,48 @@ function calc() {
     }
 
     if (gender === 'male'){ 
-        alert(days[dates.getDay()] + ',' + " your name is: " + males[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[dates.getDay()] + " . ");
+        alert("Your name is: " + males[date.getDay()]);
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[date.getDay()] + " . ");
 
     } else if (gender === 'female') {
-        alert(days[dates.getDay()] + ',' + " your name is: " + females[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[dates.getDay()] + " . ");
+        alert("Your name is: " + females[date.getDay()]);
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[date.getDay()] + " . ");
 
     } else {}
 
 }
  
+
+// function getTheDay(aText)
+
+// {
+
+// myDays=
+
+// ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+// myDate=new Date(eval('"'+aText.value+'"'))
+
+// document.form1.textDay.value=myDays[myDate.getDay()]
+
+// }
+
+
+// var ysel = document.getElementsByName("year")[0],
+// msel = document.getElementsByName("month")[0],
+// dsel = document.getElementsByName("day")[0];
+// for (var i = 2018; i >= 1900; i--) {
+//     var opt = new Option();
+//     opt.value = opt.text = i;
+//     ysel.add(opt);
+// }
+// ysel.addEventListener("change", validate_date);
+// msel.addEventListener("change", validate_date);
+// setInitialDays();
+// jQuery("form.dayname-form").submit(function (event) {
+//     var fulldate = jQuery(this).find("#month").val()+"/"+jQuery(this).find("#day").val()+"/"+jQuery(this).find("#year").val();
+//     console.log(fulldate);
+    
+//     jQuery(".dayname span").html(DayName(fulldate,jQuery(this).find("#gender").val()));		
+//     event.preventDefault();
+// });
